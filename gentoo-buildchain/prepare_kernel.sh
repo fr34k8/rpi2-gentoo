3f9d8f9109cdbefba6c7bc8fd7049cccb2399fb8
ZappeL <zappel@simple-co.de>
2015-02-08 13:19:57 +0100

    added kernel-rebuilding support.

--- gentoo-buildchain/prepare_kernel.sh
+++ gentoo-buildchain/prepare_kernel.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 oPath=$PWD
+newInst=0
 echo -e "getting kernel sources via git..."
 if [[ ! -d "./mnt/gentoo/usr/src/linux/.git" ]]; then
 	$(cd ./mnt/gentoo/usr/src && git clone -b rpi-3.18.y git://github.com/raspberrypi/linux.git)
+	newInst=1
 else
 	$(cd ./mnt/gentoo/usr/src/linux && git pull)
 fi
 echo -e "building kernel..."
 cd ./mnt/gentoo/usr/src/linux
-make ARCH=arm bcm2709_defconfig
-make ARCH=arm CROSS_COMPILE=/usr/bin/armv7a-hardfloat-linux-gnueabi- oldconfig
+if [[ ${newInst} == 1 ]]; then
+	make ARCH=arm bcm2709_defconfig
+	make ARCH=arm CROSS_COMPILE=/usr/bin/armv7a-hardfloat-linux-gnueabi- oldconfig
+else
+	make ARCH=arm CROSS_COMPILE=/usr/bin/armv7a-hardfloat-linux-gnueabi- menuconfig
+fi
 make ARCH=arm CROSS_COMPILE=/usr/bin/armv7a-hardfloat-linux-gnueabi- -j8
 make ARCH=arm CROSS_COMPILE=/usr/bin/armv7a-hardfloat-linux-gnueabi- modules_install INSTALL_MOD_PATH=${oPath}/mnt/gentoo/
 cd $oPath
